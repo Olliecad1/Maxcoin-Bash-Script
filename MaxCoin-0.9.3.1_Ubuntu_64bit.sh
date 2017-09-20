17a42fad74aab539d45d3c006efee369a453ec69
Oliver Morris <officialgamermorris@gmail.com>
2017-09-20 16:29:48 +0100

    Update MaxCoin-0.9.3.1_Ubuntu_64bit.sh

--- MaxCoin-0.9.3.1_Ubuntu_64bit.sh
+++ MaxCoin-0.9.3.1_Ubuntu_64bit.sh
@@ -5,7 +5,6 @@ wget https://github.com/Max-Coin/maxcoin/archive/v0.9.3.1.tar.gz
 tar -zxvf v0.9.3.1.tar.gz
 cd maxcoin-0.9.3.1/src/
 sudo sed -i 's/<const\ CScriptID\&/<CScriptID/' rpcrawtransaction.cpp
-sudo sed -i 's/\/usr\/lib\/libminiupnpc.a/\/usr\/lib\/i386-linux-gnu\/libminiupnpc.a/' makefile.unix
 make -f makefile.unix
 strip maxcoind
 sudo mv maxcoind /usr/bin
@@ -13,6 +12,6 @@ cd ..
 qmake maxcoin-qt.pro
 make
 mkdir ~/.maxcoin
-cd .maxcoin/
+cd ~/.maxcoin/
 touch maxcoin.conf
 printf 'addnode=213.179.202.19\n  addnode=wombat.dar.sh\n addnode=94.23.80.59\n  addnode=198.50.155.236\n server=1\n rpcuser=maxcoinrpc\n rpcpassword=examplepassword\n rpcport=8080'  >> maxcoin.conf
